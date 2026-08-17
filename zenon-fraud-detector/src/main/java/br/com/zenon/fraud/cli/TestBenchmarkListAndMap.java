9c2ac6675de0d2db9fda7c05eae9454f66a0e1d5
leonardozanol <leozianni@outlook.com>
2026-08-17 09:09:40 -0300

    feat: adiciona benchmark do tempo de busca da transacao pelo cliente na List

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/cli/TestBenchmarkListAndMap.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/cli/TestBenchmarkListAndMap.java
@@ -7,14 +7,21 @@ import java.util.Optional;
 
 public class TestBenchmarkListAndMap {
 
+    private static final TransactionListRepository transactionListRepository = new TransactionListRepository("data/PS_20174392719_1491204439457_log.csv");
+
     static void main() {
         showTransactionByNameCustomer("C12345");
         showTransactionByNameCustomer("C1231006815");
+
+        long startTime = System.nanoTime();
+        transactionListRepository.getByNameCustomer("C1868032458");
+        long endTime = System.nanoTime();
+
+        System.out.println("Tempo Benchmark: " + (endTime - startTime));
+
     }
 
     private static void showTransactionByNameCustomer(String nameCustomer) {
-        TransactionListRepository transactionListRepository = new TransactionListRepository("data/PS_20174392719_1491204439457_log.csv");
-
         Optional<Transaction> transaction = transactionListRepository.getByNameCustomer(nameCustomer);
         if (transaction.isPresent()) {
             System.out.println("Transação Encontrada:\n - " + transaction.get());
