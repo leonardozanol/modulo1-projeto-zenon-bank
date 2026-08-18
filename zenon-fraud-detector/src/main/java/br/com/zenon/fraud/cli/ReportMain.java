7dea3036e3f39f4433950db136e7d92935b7aa62
leonardozanol <leozianni@outlook.com>
2026-08-18 19:18:48 -0300

    fix: corrige forma de definir o locale

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/cli/ReportMain.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/cli/ReportMain.java
@@ -2,10 +2,6 @@ package br.com.zenon.fraud.cli;
 
 import br.com.zenon.fraud.utils.report.Statistics;
 import br.com.zenon.fraud.utils.report.TransactionReport;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 
 import java.text.NumberFormat;
 import java.util.Locale;
@@ -13,43 +9,29 @@ import java.util.ResourceBundle;
 
 public class ReportMain {
 
-    private static final Locale en = Locale.of("en", "us");
-    private static final Locale pt_BR = Locale.of("pt", "br");
-
     static void main(String[] args) {
 
-        Locale locale = en;
-
-        Options options = new Options();
-
-        options.addOption("pt_br", "pt_br", false, "Set the application locale to PT_BR");
-        options.addOption("en", "en", false, "Set the application locale to EN");
-
-        try {
-            CommandLine commandLine = new DefaultParser().parse(options, args);
-
-            if (commandLine.hasOption("pt_br")) {
-                locale = pt_BR;
-            }
-
-            if (commandLine.hasOption("en")) {
-                locale = en;
-            }
-
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
+        Locale locale = setLocale(args);
 
         ResourceBundle resourceBundle = ResourceBundle.getBundle("report", locale);
-        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(locale);
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(locale);
+        NumberFormat integerFormat = NumberFormat.getInstance(locale);
 
         TransactionReport transactionReport = new TransactionReport();
         Statistics statistics = transactionReport.report("data/PS_20174392719_1491204439457_log.csv");
 
-        System.out.println(resourceBundle.getString("report.main.totalLines") + ": " + statistics.totalLines());
-        System.out.println(resourceBundle.getString("report.main.totalFrauds") + ": " + statistics.totalFraud());
-        System.out.println(resourceBundle.getString("report.main.totalValue") + ": " + numberFormat.format(statistics.totalAmount()));
+        System.out.println(resourceBundle.getString("report.main.totalLines") + ": " + integerFormat.format(statistics.totalLines()));
+        System.out.println(resourceBundle.getString("report.main.totalFrauds") + ": " + integerFormat.format(statistics.totalFraud()));
+        System.out.println(resourceBundle.getString("report.main.totalValue") + ": " + currencyFormat.format(statistics.totalAmount()));
 
+    }
+
+    private static Locale setLocale(String[] args) {
+        if (args.length > 0 && args[0].equals("--pt_br")) {
+            return Locale.of("pt", "br");
+        }
+
+        return Locale.of("en", "us");
     }
 
 }
