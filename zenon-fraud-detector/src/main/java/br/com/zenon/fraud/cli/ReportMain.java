a94b86e016eab232d609b9dc547e280284f4d2c7
leonardozanol <leozianni@outlook.com>
2026-08-19 19:55:31 -0300

    fix: corrige formatação de moeda utilizando DecimalFormat e definindo a moeda para USD

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/cli/ReportMain.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/cli/ReportMain.java
@@ -3,7 +3,9 @@ package br.com.zenon.fraud.cli;
 import br.com.zenon.fraud.utils.report.Statistics;
 import br.com.zenon.fraud.utils.report.TransactionReport;
 
+import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.Currency;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -14,21 +16,23 @@ public class ReportMain {
         Locale locale = setLocale(args);
 
         ResourceBundle resourceBundle = ResourceBundle.getBundle("report", locale);
-        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(locale);
-        NumberFormat integerFormat = NumberFormat.getInstance(locale);
+        NumberFormat integerFormatter = NumberFormat.getInstance(locale);
+
+        NumberFormat currencyFormatter = DecimalFormat.getCurrencyInstance(locale);
+        currencyFormatter.setCurrency(Currency.getInstance("USD"));
 
         TransactionReport transactionReport = new TransactionReport();
         Statistics statistics = transactionReport.report("data/PS_20174392719_1491204439457_log.csv");
 
         String labelTotalLines = resourceBundle.getString("report.main.totalLines");
         String labelTotalFrauds = resourceBundle.getString("report.main.totalFrauds");
-        String labelTotalValue = resourceBundle.getString("report.main.totalValue");
+        String labelTotalAmount = resourceBundle.getString("report.main.totalValue");
 
-        String formattedTotalLines = integerFormat.format(statistics.totalLines());
-        String formattedTotalFrauds = integerFormat.format(statistics.totalFraud());
-        String formattedTotalValue = currencyFormat.format(statistics.totalAmount());
+        String formattedTotalLines = integerFormatter.format(statistics.totalLines());
+        String formattedTotalFrauds = integerFormatter.format(statistics.totalFraud());
+        String formattedTotalAmount = currencyFormatter.format(statistics.totalAmount());
 
-        System.out.printf("%s: %s%n%s: %s%n%s: %s%n", labelTotalLines, formattedTotalLines, labelTotalFrauds, formattedTotalFrauds, labelTotalValue, formattedTotalValue);
+        System.out.printf("%s: %s%n%s: %s%n%s: %s%n", labelTotalLines, formattedTotalLines, labelTotalFrauds, formattedTotalFrauds, labelTotalAmount, formattedTotalAmount);
 
     }
 
